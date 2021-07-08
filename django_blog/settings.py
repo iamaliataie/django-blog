4989eb630007d2a38c23d00a6fd61e1fada0c321
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2021-07-08 08:04:45 +0430

    django_on_heroku

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import django_on_heroku
 from pathlib import Path
 from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -151,3 +153,4 @@ EMAIL_HOST_PASSWORD = config('PASSWORD', default='')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+django_on_heroku.settings(locals())
