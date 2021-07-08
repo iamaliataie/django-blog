4b1aef794a326154aa0e48be43fb3cf793054eb7
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2021-07-08 18:01:36 +0430

    profile form updated

--- users/forms.py
+++ users/forms.py
@@ -28,6 +28,13 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email']
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        email_exists = User.objects.filter(email=email).exists()
+        if email_exists:
+            raise forms.ValidationError('A user with that email already exists.')
+        return email
 
 
 class ProfileUpdateForm(forms.ModelForm):
