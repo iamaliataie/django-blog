59d1056f821d4a9b04d608ec3f191193a9e4f75a
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2021-07-13 15:47:57 +0430

    password change feature added

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -22,15 +22,27 @@ from users import views as user_views
 
 
 urlpatterns = [
+
+    # blog urls
     path('', include('blog.urls')),
+
+    # authenticating urls
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    
+    # password change urls
+    path('password-change/', auth_views.PasswordChangeView.as_view(template_name='users/password_change.html'), name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
+    
+    # password reset urls
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
     path('password-reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
     path('password-reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
+
+    # Django admin panel urls
     path('admin/', admin.site.urls),
 ]
 
